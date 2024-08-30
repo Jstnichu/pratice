e983c596d4765cd5d3545d1fd34da0bc8b8f993a
Sure Nichith <sure.nichith_2025@woxsen.edu.in>
2024-08-31 00:07:11 +0530

    inital commit

--- app.js
+++ app.js
@@ -4,6 +4,23 @@ const {MongoClinet} = require('mongodb');
 const app = express();
 const port = 5000
 
+dotenv.config();
+MongoClinet.connect(process.env.MONGO_URI, (err, client) => {   
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log('Connected to MongoDB');
+    const db = client.db('test');
+    const collection = db.collection('test');
+    collection.insertOne({name: 'test'}, (err, result) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log('Document inserted');
+    });
+});
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
